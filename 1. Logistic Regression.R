517ba41f9ac5b4443fbd8513157103eb1db6a603
BrundhaR <54657511+BrundhaR@users.noreply.github.com>
2019-11-22 20:03:32 +0530

    Update 1. Logistic Regression.R

--- 1. Logistic Regression.R
+++ 1. Logistic Regression.R
@@ -12,25 +12,34 @@ data  <- read.csv("/Users/brundhar/Desktop/DSR Project/Shortcreditcard.csv")    
 data$Amount=scale(data$Amount)
 data$Time=scale(data$Time)
 
-NewData<-data[,-c(1)]
+set.seed(123)
+data_sample = sample.split(data$Class,SplitRatio=0.80)
+train_data = subset(data,data_sample==TRUE)
+train_data = train_data[,-c(1)]
+test_data = subset(data,data_sample==FALSE)
+test_data_class=test_data$Class
+test_data = test_data[,-c(1)]
+dim(train_data)
+dim(test_data)
 
 start_time <- Sys.time()     #start timer
 
 #to fit logistic regression model
-Logistic_Model=glm(Class~.,NewData,family=binomial(link = "logit"), maxit=100)
+Logistic_Model=glm(Class~.,train_data,family=binomial(link = "logit"), maxit=100)
 #Logistic_Model=bayesglm(Class~.,NewData,family=binomial(), maxit=100)
 summary(Logistic_Model)
+
 end_time <- Sys.time()      #stop timer
 
 cat ("Time taken to build model:", (end_time - start_time))
 
 plot(Logistic_Model)
 
-exit<-data$Class
-var2<-cbind.data.frame( NewData$Time, NewData$Amount, exit )
+class<-test_data_class
+var2<-cbind.data.frame( test_data$Time, test_data$Amount, class )
 
-pred<-predict(Logistic_Model,type="response")
-predobj<-prediction(pred,var2$exit)
+pred<-predict(Logistic_Model,newdata=test_data, type="response")
+predobj<-prediction(pred,var2$class)
 rocobj<-performance(predobj,measure="tpr",x.measure="fpr")
 aucobj<-performance(predobj,measure="auc")
 
