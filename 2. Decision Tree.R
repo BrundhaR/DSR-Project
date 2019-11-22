516df665d425433fd6eed9fcbf9e3a31774a8852
BrundhaR <54657511+BrundhaR@users.noreply.github.com>
2019-11-22 19:56:46 +0530

    Update 2. Decision Tree.R

--- 2. Decision Tree.R
+++ 2. Decision Tree.R
@@ -9,23 +9,37 @@ data  <- read.csv("/Users/brundhar/Desktop/DSR Project/Shortcreditcard.csv")    
 
 NewData<-data[,-c(1)]
 
+set.seed(123)
+data_sample = sample.split(data$Class,SplitRatio=0.80)
+train_data = subset(data,data_sample==TRUE)
+train_data = train_data[,-c(1)]
+test_data = subset(data,data_sample==FALSE)
+test_data_class=test_data$Class
+test_data = test_data[,-c(1)]
+dim(train_data)
+dim(test_data)
+
 #DECISION TREE
 start_time_d <- Sys.time()
-decisionTree_model <- rpart(Class ~ . , NewData, method = 'class', control=rpart.control(minsplit=1), parms=list(split='information'))
-predicted_val <- predict(decisionTree_model, NewData, type = 'class')
-probability <- predict(decisionTree_model, NewData, type = 'prob')
+decisionTree_model <- rpart(Class ~ . , train_data, method = 'class', control=rpart.control(minsplit=1), parms=list(split='information'))
+predicted_val <- predict(decisionTree_model, train_data, type = 'class')
+probability <- predict(decisionTree_model, train_data, type = 'prob')
 rpart.plot(decisionTree_model, type=2)
 end_time_d <- Sys.time()
 
-cat ("Time taken to build model:", (end_time - start_time))
+cat ("Time taken to build model:", (end_time_d - start_time_d))
 
-exit<-data$Class
-var2<-cbind.data.frame( NewData$Time, NewData$Amount, exit )
+class<-test_data_class
+head(class)
+var2<-cbind.data.frame( test_data$Time, test_data$Amount, class )
+pred<-predict(decisionTree_model,newdata=test_data, type="prob")[,2]
 
-pred<-predict(Logistic_Model,type="response")
-predobj<-prediction(pred,var2$exit)
+predobj<-prediction(pred,var2$class)
 rocobj<-performance(predobj,measure="tpr",x.measure="fpr")
 aucobj<-performance(predobj,measure="auc")
 
-plot(rocobj,main=paste("Area under curve",round(aucobj@y.values[[1]],4)))
+plot(rocobj,main=paste("Area under curve",round(aucobj@y.values[[1]],6)))
 aucobj@y.values
+
+
+
